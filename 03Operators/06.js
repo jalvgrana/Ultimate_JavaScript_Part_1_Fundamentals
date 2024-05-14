c32c9f1046cd5e169a9ef368ac9a236d8dfb8fc1
jalvgrana <63903109+jalvgrana@users.noreply.github.com>
2024-05-14 12:49:16 +0200

    Add files via upload

---
+++ 03Operators/06.js
@@ -0,0 +1,5 @@
+// Ternary operators
+
+let number = 100;
+let type = number > 90 ? 'gold' : 'silver';
+console.log(type)
---
+++ 03Operators/08.js
@@ -0,0 +1,14 @@
+// Logical Operators with Non-booleans
+
+console.log(false || true) // true
+console.log(false || 'Mosh') // Mosh 
+console.log(false || 1) // 1
+
+/*
+Falsy (false)
+undefined
+null
+0
+false
+''
+*/
